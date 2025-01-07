b8e92d21c813be35aaf592ee21ec2c667d281f5e
George Ferreira <gehwallace@gmail.com>
2025-01-07 18:24:17 -0300

    Criando a class Model

---
+++ src/main/java/com/api/cadastrocliente/model/Cliente.java
@@ -0,0 +1,27 @@
+package com.api.cadastrocliente.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.io.Serializable;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "TB_CLIENTE")
+public class Cliente implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true, length = 50)
+    private String nome;
+
+}
